8331b9c679f03c005dba10b8ff7a544083a44a0f
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-07-17 15:33:00 +0700

    Perbaikan

--- Semester 2/UPM/Pemrograman/Soal 2022/Nomor2.java
+++ Semester 2/UPM/Pemrograman/Soal 2022/Nomor2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Nomor22 {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     
     // Input Sebuah Angka
