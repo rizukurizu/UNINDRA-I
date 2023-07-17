7b2ed204bc473bb132ccd88f7fe568e8c18f2fd7
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-07-17 16:07:13 +0700

    delete scanner

--- Semester 2/UPM/Pemrograman/Soal 2021/Nomor2_a.java
+++ Semester 2/UPM/Pemrograman/Soal 2021/Nomor2_a.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-
 public class Nomor2_a {
     public static void main (String[] args) {
-        Scanner in = new Scanner(System.in);
-        
         int hitung = 1;
         for (int baris = 0; baris < 5; baris++) {
             for (int kolom = 0; kolom < baris + 1; kolom++) {
