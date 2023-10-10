5a08426ef93dbbf20fa2e510b00c7ee721cedfcd
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-10-10 18:33:09 +0700

    Update Pertemuan_1.java

--- Semester 3/Latihan/Pemrograman/Pertemuan 1/Pertemuan_1.java
+++ Semester 3/Latihan/Pemrograman/Pertemuan 1/Pertemuan_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pertemuan1 {
+public class Pertemuan_1 {
     public static void main (String[] args) {
         System.out.println("Tugas 1: Dasar Pemrograman Java");
         System.out.println("Nama : Tobi Saputra");
