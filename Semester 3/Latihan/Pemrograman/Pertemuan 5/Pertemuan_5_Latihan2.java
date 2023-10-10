1cd1c1d27c7bff3fe399524d24ada2bf6ad6c02e
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-10-10 18:39:42 +0700

    Create Pertemuan_5_Latihan2.java

---
+++ Semester 3/Latihan/Pemrograman/Pertemuan 5/Pertemuan_5_Latihan2.java
@@ -0,0 +1,13 @@
+public class Pertemuan_5_Latihan2 {
+    public static void main(String[] args) {
+        System.out.println("Tugas 4: Operasi String");
+        System.out.println("Nama : Tobi Saputra");
+        System.out.println("NPM : 202243502612");
+        System.out.println();
+        String str = "Pemrograman Java";
+        
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.substring(i, i + 1));
+        }
+    }
+}
