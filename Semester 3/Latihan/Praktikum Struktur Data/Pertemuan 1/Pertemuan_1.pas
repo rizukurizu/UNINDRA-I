794dfa3857d83fc497f09fd217ddf66206fa7b6b
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-10-12 20:45:05 +0700

    Create Pertemuan_1.pas

---
+++ Semester 3/Latihan/Praktikum Struktur Data/Pertemuan 1/Pertemuan_1.pas
@@ -0,0 +1,12 @@
+program Pertemuan_1;
+uses crt;
+
+var nama, npm: string;
+
+begin
+    clrscr;
+    write('Masukkan Nama Anda : '); readln(nama);
+    write('Masukkan NPM Anda : '); readln(npm);
+    writeln('Halo ', nama, ' ', npm);
+    readln;
+end.    
