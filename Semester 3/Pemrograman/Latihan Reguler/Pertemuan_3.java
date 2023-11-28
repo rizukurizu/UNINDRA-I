acde02a02e49cf024de851cc003071694aa19f77
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-11-28 10:39:55 +0700

    Update Pertemuan_3.java

--- Semester 3/Pemrograman/Latihan Reguler/Pertemuan_3.java
+++ Semester 3/Pemrograman/Latihan Reguler/Pertemuan_3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pertemuan_3 {
+class Pertemuan_3 {
     public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
         int jumlah;
