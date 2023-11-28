7668435c675c0c18aa81dd738348dfc0ccc8a3ac
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-11-28 10:40:48 +0700

    Update Pertemuan_1.java

--- Semester 3/Pemrograman/Latihan Reguler/Pertemuan_1.java
+++ Semester 3/Pemrograman/Latihan Reguler/Pertemuan_1.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 class Pertemuan_1 {
     public static void main (String[] args) {
+        System.out.println("Tugas 1: Dasar Pemrograman Java");
+        System.out.println("Nama : Tobi Saputra");
+        System.out.println("NPM : 202243502612");
+        System.out.println();
+        
         Scanner in = new Scanner(System.in);
         
         // 4 Variable
         int angka1, angka2, hasil; 
-        boolean status;
         
         System.out.println("---==[ PERKALIAN ]==---");
         System.out.print("Masukan Angka 1 : ");
@@ -23,5 +27,6 @@ class Pertemuan_1 {
         if (hasil == 0) System.out.println("Menghasilkan Angka Genap");
         else System.out.println("Menghasilkan Angka Ganjil");
         
+        in.close();
     }
 }
